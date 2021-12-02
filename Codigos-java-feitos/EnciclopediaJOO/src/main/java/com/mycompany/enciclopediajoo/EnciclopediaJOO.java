7721360826b8e64e4eb72d7054ad09cfbbd7db28
Israel Ferreira <israel.fnds@hotmail.com>
2021-12-02 16:13:18 -0300

    att

---
+++ Codigos-java-feitos/EnciclopediaJOO/src/main/java/com/mycompany/enciclopediajoo/EnciclopediaJOO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.enciclopediajoo;
+
+/**
+ *
+ * @author israe
+ */
+public class EnciclopediaJOO {
+    // objeto = característica + comportamento + estado
+    
+    //classe = molde de objetos  / o que eu tenho, o que eu faço e como está agora
+    //classe = atributos / metodos / estado
+    
+    /*
+    Classe Caneta
+    modelo: Caractere
+    cor: Caractere
+    ponta: Inteiro
+    carga: Inteiro
+    tampada: Logico
+    
+    Metodo rabiscar()
+    Se(tampada) entao
+       Escreva ("ERRO")
+    senao
+       Escreva("Rabisco")
+    FimSe
+    FimMetodo
+    Metodo tampar()
+       tampada = verdadeiro
+    FimMetodo
+    FimClasse
+    */
+    
+    /* Instanciar é gerar um objeto a partir de uma classe (estou instanciando um objeto a partir de uma classe)
+    c1 = nova Caneta
+    c1.cor = "Azul"
+    c1.ponta = "0,5"
+    c1.tampada = falso
+    c1.rabiscar()
+    
+    c2 = nova Caneta
+    c2.cor = "Vermelho"
+    c2.ponta = "1,0"
+    c2.tampada = falso
+    c2.tampar()
+    */
+    
+    /*
+    Classe: define atributos e métodos comuns que serão compartilhados por um objeto
+    Objeto: É uma instância de uma classe
+    */
+    
+    /*
+    
+    Visibilidade
+    
+    + publico    - qualquer um pode usar (classe atual e todas as outras classes)
+    - privado      - somente eu posso ver/usar  (somente a classe atual)
+    # protegido       telefone fixo (apenas para familia)   (a classe atual e a todas as suas sub-classes)
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    /**/
+    
+}
