512e9ec009833f891985805199e082a4cdf3cff9
Israel Ferreira <israel.fnds@hotmail.com>
2021-12-03 16:06:20 -0300

    Update EnciclopediaJOO.java

--- Codigos-java-feitos/EnciclopediaJOO/src/main/java/enciclopediajoo/EnciclopediaJOO.java
+++ Codigos-java-feitos/EnciclopediaJOO/src/main/java/enciclopediajoo/EnciclopediaJOO.java
@@ -14,7 +14,7 @@ public class EnciclopediaJOO {
     //classe = molde de objetos  / o que eu tenho, o que eu faço e como está agora
     //classe = atributos / metodos / estado
     
-    /*
+    /* AULA 01
     Classe Caneta
     modelo: Caractere
     cor: Caractere
@@ -35,7 +35,8 @@ public class EnciclopediaJOO {
     FimClasse
     */
     
-    /* Instanciar é gerar um objeto a partir de uma classe (estou instanciando um objeto a partir de uma classe)
+    /* AULA 02
+    Instanciar é gerar um objeto a partir de uma classe (estou instanciando um objeto a partir de uma classe)
     c1 = nova Caneta
     c1.cor = "Azul"
     c1.ponta = "0,5"
@@ -49,12 +50,12 @@ public class EnciclopediaJOO {
     c2.tampar()
     */
     
-    /*
+    /* AULA 03
     Classe: define atributos e métodos comuns que serão compartilhados por um objeto
     Objeto: É uma instância de uma classe
     */
     
-    /*
+    /* AULA 04
     
     Visibilidade
     
@@ -64,9 +65,59 @@ public class EnciclopediaJOO {
     
     */
     
+    /* AULA 05
+    Métodos especiais
     
+    Métodos acessores
+    Getters (pega alguma coisa mantendo a segurança de acesso - get ou is)
+    t = e.getTotDoc();
     
+    Métodos modificadores
+    Setters (coloca alguma coisa mantendo a segurança de acesso - set )
+    e.setTotDoc(doc);
     
+    Método Construtor
+    Construct (executa tarefa sem que o usuário faça chamada)
+    
+    Metodo construtor()
+    tampar()
+    cor = "Azul"
+    FimMetodo
+    
+    Metodo construtor(m: caractere, c: caractere, p:real)
+    setModelo(m)
+    setCor(c)
+    setPonta(p)
+    tampar()
+    FimMetodo
+    
+    */
+    
+    /* AULA 06
+    Classe - Atributo - Método - Estado
+    
+    Encapsulamento
+    Herança
+    Polimorfismo
+    
+    EHP
+    
+    Encapsulamento - capsula (envólocro) - serve de proteção / criar padrão 
+    Encapsular é ocultar partes independentes da implementação, permitindo construir partes invisíveis ao mundo exterior
+    
+    Mandamos mensagens pra capsula (objeto) e ela envia uma resposta
+    
+    Bom objeto encapsulado possui uma boa interface, lista de serviços - comunicação com o meio externo
+    
+    - Tornar mudanças invisíveis
+    - Facilitar a reutilização de códigos
+    - Reduzir efeitos colaterais
+    
+    Interface parece classe mas não tem atributos, apenas métodos
+    
+    Geralmente na interface todos os métodos são publicos
+    
+    */
     
     
     
